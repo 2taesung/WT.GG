d9aae2131f7cde17d452168255481e95dccc0b50
이태성 <dlxotjde@gmail.com>
2021-10-28 20:48:52 +0900

    [FE] feat: Test 미완성이지만 일단...!

--- frontend/src/Components/Test/Test.tsx
+++ frontend/src/Components/Test/Test.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './Test.css';
 
 function Test() {
   return (
-    <div>
-      성향테스트 시우시땅
+    <div id="bg">
+      
+
+      <div className="card">
+        <div className="gage">
+
+        </div>
+        <div className="Q">
+          <h1>나는 아침에 일어나면 제일 먼저..</h1>
+        </div>
+        <div>
+          <img src="../../public/image/favicon.png" alt=""/>
+        </div>
+        
+        <div className="a">
+          <h2>양치를 한다.</h2>
+        </div>
+        <br />
+        <div className="a">
+          <h2>세수를 한다.</h2>
+        </div>
+
+      </div>
+      
     </div>
   )
 }
