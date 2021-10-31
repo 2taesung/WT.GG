15eeca0f8f76d2bcafb5903d6e78ef9f2c80a32a
이태성 <dlxotjde@gmail.com>
2021-10-31 16:08:03 +0900

    [FE] feat:cazoul v1

--- frontend/src/Components/Test/Test.tsx
+++ frontend/src/Components/Test/Test.tsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './slickstyle.css'; 
+import './slick-theme.css';
 import './Test.css';
 
-function Test() {
-  return (
-    <div id="bg">
-      
 
-      <div className="card">
-        <div className="gage">
-
-        </div>
-        <div className="Q">
-          <h1>나는 아침에 일어나면 제일 먼저..</h1>
-        </div>
-        <div>
-          <img src="../../public/image/favicon.png" alt=""/>
-        </div>
-        
-        <div className="a">
-          <h2>양치를 한다.</h2>
-        </div>
-        <br />
-        <div className="a">
-          <h2>세수를 한다.</h2>
-        </div>
-
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div className="bg">
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
       </div>
-      
-    </div>
-  )
+    );
+  }
 }
-
-export default Test;
