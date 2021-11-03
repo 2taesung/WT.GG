2277bb7449d26d01177f4dc164ee5d82bf3e6d3f
Jeonguk Kim <jukim10101@gmail.com>
2021-11-03 11:02:42 +0900

    [BE] feat : board CRUD code

---
+++ backend/webtoon/src/main/java/com/web/webtoon/model/Comment.java
@@ -0,0 +1,65 @@
+package com.web.webtoon.model;
+
+import java.sql.Timestamp;
+
+public class Comment {
+	private int id;
+	private int ip_id;
+	private int post_id;
+	private int platform_id;
+	private int webtoon_id;
+	private String content;
+	private String writer;
+	private Timestamp date;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIp_id() {
+		return ip_id;
+	}
+	public void setIp_id(int ip_id) {
+		this.ip_id = ip_id;
+	}
+	public int getPost_id() {
+		return post_id;
+	}
+	public void setPost_id(int post_id) {
+		this.post_id = post_id;
+	}
+	public int getPlatform_id() {
+		return platform_id;
+	}
+	public void setPlatform_id(int platform_id) {
+		this.platform_id = platform_id;
+	}
+	public int getWebtoon_id() {
+		return webtoon_id;
+	}
+	public void setWebtoon_id(int webtoon_id) {
+		this.webtoon_id = webtoon_id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Timestamp getDate() {
+		return date;
+	}
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+	
+	
+}
--- backend/webtoon/src/main/java/com/web/webtoon/model/service/BoardService.java
+++ backend/webtoon/src/main/java/com/web/webtoon/model/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.web.webtoon.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.web.webtoon.model.BoardContent;
 
@@ -9,4 +10,10 @@ public interface BoardService {
 	public List<BoardContent> getAllList() throws Exception;
 	
 	public BoardContent getBoardContent(int id) throws Exception;
+	
+	public int insertBoardContent(Map map) throws Exception;
+	
+	public int modifyBoardContent(Map map) throws Exception;
+	
+	public int deleteBoardContent(int id) throws Exception;
 }
